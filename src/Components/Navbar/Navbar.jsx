225d6fd9641d32c64640e26e4b9a01b810b5a527
Fidha07 <120751629+Fidha07@users.noreply.github.com>
2023-12-25 05:09:08 +0530

    Update package-lock.json, package.json, public/index.html, src/App.js, src/Components/Navbar/Navbar.jsx, and src/index.css (generated by blackbox.ai )

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
 
 const Navbar = () => {
+
+      const [menu,setMenu] = useState("Shop")
+
     return (
         <div className='navbar'>
           <div className="nav-logo">
             <img src={process.env.PUBLIC_URL + '/logo1.png'} alt="Logo" className="logo"/>
-             <p>SHOPPER</p>
+             <p>CUT & NEEDLE</p>
           </div>
           <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Women</li>
-            <li>Girls</li>
-            <li>Accessories</li>
+            <li onClick={()=>{setMenu("shops")}}>Shop{menu==="shops"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("girls")}}>Girls{menu==="girls"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("accessories")}}>Accessories{menu==="accessories"?<hr/>:<></>}</li>
           </ul>
           <div className="nav-login-cart">
             <button>Login</button>
             <img src={process.env.PUBLIC_URL + '/cart_icon.png'} alt="" />
+            <div className="nav-cart-count">0</div>
           </div>
         </div>
     )
